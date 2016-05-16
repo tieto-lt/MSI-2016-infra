f3440f8327846597e7c33445e80fbd5b26d19476
Antanas Bastys <antanas.bastys@tieto.com>
2016-05-16 18:58:26 +0300

    Updated wireless

--- wireless.sh
+++ wireless.sh
@@ -1,10 +1,10 @@
 drone_sid="ardrone2_128219"
 wifi_sid="extOpenWrt"
 
-nmcli d disconnect wlan0
-nmcli d wifi connect $drone_sid ifname wlan0
+nmcli c down id $wifi_sid
+nmcli c up $drone_sid
 
 echo "killall udhcpd; iwconfig ath0 mode managed essid $wifi_sid; ifconfig ath0 192.168.16.1 netmask 255.255.255.0 up;" | telnet 192.168.1.1
 
-nmcli d disconnect wlan0
-nmcli d wifi connect $wifi_sid ifname wlan0
+nmcli c down $drone_sid
+nmcli c up $wifi_sid
