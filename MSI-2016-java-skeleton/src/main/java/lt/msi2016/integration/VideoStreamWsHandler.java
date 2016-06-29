d39446706e17b97fbed903b27f17ba5ea6bd26c4
Zygimantas Gatelis <zygis.gg@gmail.com>
2016-06-29 11:33:21 +0300

    Removed video converter stuff

--- MSI-2016-java-skeleton/src/main/java/lt/msi2016/integration/VideoStreamWsHandler.java
+++ MSI-2016-java-skeleton/src/main/java/lt/msi2016/integration/VideoStreamWsHandler.java
@@ -1,10 +1,7 @@
 package lt.msi2016.integration;
 
-import lt.msi2016.video.VideoStreamRegistry;
-import lt.msi2016.ws.WsUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketSession;
@@ -15,27 +12,21 @@ public class VideoStreamWsHandler extends BinaryWebSocketHandler {
 
     private static Logger LOG = LoggerFactory.getLogger(VideoStreamWsHandler.class);
 
-    @Autowired
-    private VideoStreamRegistry videoRegistry;
-
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         session.setBinaryMessageSizeLimit(1000000);
         LOG.info("Got connection from {}", session.getUri());
-        videoRegistry.startRecording(WsUtils.getOperatorToken(session)); // TODO: this should be on mission start
     }
 
     @Override
     protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
         LOG.info("Received video bytes: {}", message.getPayloadLength());
 
-        videoRegistry.record(WsUtils.getOperatorToken(session), message.getPayload());
         super.handleBinaryMessage(session, message);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        videoRegistry.stopRecording(WsUtils.getOperatorToken(session)); // TODO: this should be on mission completed.
         super.afterConnectionClosed(session, status);
     }
 }
